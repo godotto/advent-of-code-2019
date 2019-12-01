02b66a02208a42c30a7d8703f894997af0ed8ec8
Maciej <maciek1234599@gmail.com>
2019-12-01 18:06:27 +0100

    Add Day 1 solution

---
+++ Day 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            Console.WriteLine(CalculateAllFuel(lines)); // answer for the first puzzle
+            Console.WriteLine(ImprovedCalculateAllFuel(lines)); // answer for the second puzzle
+        }
+
+        static int CalculateFuel(int mass)
+        {
+            return (mass / 3) - 2; // fuel required for one module of a certain mass
+        }
+
+        static int CalculateAllFuel(string[] masses)
+        {
+            var sumOfFuel = 0;
+            foreach (var line in masses)
+                sumOfFuel += CalculateFuel(Convert.ToInt32(line));
+            return sumOfFuel;
+        }
+
+        static int ImprovedCalculateFuel(int mass)
+        {
+            int calculatedMass = mass / 3 - 2;
+            if (calculatedMass <= 0) // if no more fuel needed returns 0
+                return 0;
+            else return calculatedMass + ImprovedCalculateFuel(calculatedMass); // calculate fuel for already calculated fuel
+        }
+
+        static int ImprovedCalculateAllFuel(string[] masses)
+        {
+            var sumOfFuel = 0;
+            foreach (var line in masses)
+                sumOfFuel += ImprovedCalculateFuel(Convert.ToInt32(line));
+            return sumOfFuel;
+        }
+    }
+}
