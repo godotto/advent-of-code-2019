4f4c9c4d5a732499a53e29481cff18d460eb46d2
Maciej <maciek1234599@gmail.com>
2019-12-02 17:29:49 +0100

    Implement ExecuteIntcode function

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -15,7 +15,18 @@ namespace Day_2
             if (operation == "1")
                 return Convert.ToInt32(first) + Convert.ToInt32(second);
             else
-                return Convert.ToInt32(first) * Convert.ToInt32(second); 
+                return Convert.ToInt32(first) * Convert.ToInt32(second);
+        }
+
+        static void ExecuteIntcode(ref string[] input)
+        {
+            for (var i = 0; i < input.Length; i += 4)
+            {
+                if (input[i] == "99")
+                    return;
+                else
+                    input[Convert.ToInt32(input[i + 3])] = Convert.ToString(Calculate(input[i], input[Convert.ToInt32(input[i + 1])], input[Convert.ToInt32(input[i + 2])]));
+            }
         }
     }
 }
