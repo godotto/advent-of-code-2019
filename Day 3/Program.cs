3421ddc721e0237a64072ebca12494341ceb2a0d
Maciej <maciek1234599@gmail.com>
2019-12-04 10:50:43 +0100

    Add auxiliary function ReadNumberOfSteps

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -10,5 +10,14 @@ namespace Day_3
             string[] line1 = input[0].Split(',');
             string[] line2 = input[1].Split(',');
         }
+
+        static int ReadNumberOfSteps(string instruction)
+        {
+            string numberOfSteps = "";
+            for (var i = 1; i < instruction.Length; i++)
+                numberOfSteps += instruction[i];
+
+            return Convert.ToInt32(numberOfSteps);
+        }
     }
 }
